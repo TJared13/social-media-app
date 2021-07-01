d4afe29bc82a8102fec436295478a27db02daee2
TJared13 <trevin.jared@gmail.com>
2021-07-01 15:09:14 -0700

    working on aws

--- server/controllers/awsController.js
+++ server/controllers/awsController.js
@@ -1,32 +1,38 @@
 require("dotenv").config();
 const fs = require("fs");
-const S3 = require("aws-sdk/clients/s3");
+const aws = require("aws-sdk");
 const { AWS_BUCKET_NAME, AWS_REGION, AWS_KEY, AWS_SECRET } = process.env;
 
-const s3 = new S3({
-	AWS_REGION,
-	AWS_KEY,
-	AWS_SECRET
+const s3 = new aws.S3({
+	accessKeyId: AWS_KEY,
+	secretAccessKey: AWS_SECRET
 });
 
-//UPLOAD TO s3
-function uploadFile(file) {
-	const fileStream = fs.createReadStream(file.path);
-	const uploadParams = {
+const uploadFile = (filename) => {
+	const fileContent = fs.readFileSync(filename);
+
+	const params = {
 		Bucket: AWS_BUCKET_NAME,
-		Body: fileStream,
-		Key: file.filename
+		Key: "photo.jpg",
+		Body: fileContent
 	};
-	return s3.upload(uploadParams).promise();
-}
+
+	s3.upload(params, (err, data) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log("File uploaded successfully", data.Location);
+		}
+	});
+};
 exports.uploadFile = uploadFile;
 
-//DOWNLOAD FROM s3
-function getFileStream(fileKey) {
-	const downloadParams = {
-		Key: fileKey,
-		Bucket: AWS_BUCKET_NAME
-	};
-	return s3.getObject(downloadParams).createReadStream();
-}
-exports.getFileStream = getFileStream;
+// //DOWNLOAD FROM s3
+// function getFileStream(fileKey) {
+// 	const downloadParams = {
+// 		Key: fileKey,
+// 		Bucket: AWS_BUCKET_NAME
+// 	};
+// 	return s3.getObject(downloadParams).createReadStream();
+// }
+// exports.getFileStream = getFileStream;
