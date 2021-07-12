8caf96eded6c6774c0115763f9cf033626c78053
TJared13 <trevin.jared@gmail.com>
2021-07-12 12:36:33 -0700

    new branch

---
+++ src/components/ParticleBackground.js
@@ -0,0 +1,6 @@
+import Particles from 'react-particles-js';
+import particlesConfig from '../config/particle-config';
+
+export default function ParticleBackground() {
+	return <Particles></Particles>;
+}
